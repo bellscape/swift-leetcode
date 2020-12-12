7343133c12bfad52c5db9333abd994359cdc8513
bell <_>
2020-12-12 21:45:38 +0800

    + p3

---
+++ Tests/p1/p3.swift
@@ -0,0 +1,50 @@
+import XCTest
+
+// Longest Substring Without Repeating Characters
+class p3: XCTestCase {
+
+
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var lastIndexDict = [Character: Int]()
+        var firstSkipped = -1
+        var maxLength = 0
+
+        for (i, c) in s.enumerated() {
+            if let lastIndex = lastIndexDict[c] {
+                firstSkipped = max(firstSkipped, lastIndex)
+            }
+            lastIndexDict[c] = i
+            maxLength = max(maxLength, i - firstSkipped)
+        }
+        return maxLength
+    }
+
+
+    func lengthOfLongestSubstring__v1(_ s: String) -> Int {
+        var lastIndexDict = [Character: Int]()
+        var from = 0
+        var maxLength = 0
+
+        let charArray = Array(s)
+        for (i, c) in charArray.enumerated() {
+            if let lastIndex = lastIndexDict[c], lastIndex >= from {
+                from = lastIndex + 1
+            }
+            lastIndexDict[c] = i
+            let len = i - from + 1
+            if maxLength < len {
+                maxLength = len
+            }
+        }
+        return maxLength
+    }
+
+
+    func testExample() throws {
+        XCTAssertEqual(lengthOfLongestSubstring("abcabcbb"), 3)
+        XCTAssertEqual(lengthOfLongestSubstring("bbbbb"), 1)
+        XCTAssertEqual(lengthOfLongestSubstring("pwwkew"), 3)
+        XCTAssertEqual(lengthOfLongestSubstring(""), 0)
+    }
+
+}
