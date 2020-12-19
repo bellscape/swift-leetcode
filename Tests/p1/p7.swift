878369fa818e1d184f931d946e3d1b98bd90c6a4
bell <_>
2020-12-19 22:12:56 +0800

    + p7

--- Tests/p1/p7.swift
+++ Tests/p1/p7.swift
@@ -6,7 +6,18 @@ class p7: XCTestCase {
     }
 
 
+    // improve: use reversed string instead of mod
     func reverse(_ x: Int) -> Int {
+        var out = Int(String(String(abs(x)).reversed()))!
+        if x < 0 {
+            out *= -1
+        }
+        return out > Int32.max || out < Int32.min ? 0 : out
+    }
+
+
+    // improve: no need to consider negative number separately
+    func reverse_v2(_ x: Int) -> Int {
         var left = x
         var out = 0
         while left != 0 {
@@ -17,7 +28,6 @@ class p7: XCTestCase {
     }
 
 
-    // no need to consider negative number separately
     func reverse_v1(_ x: Int) -> Int {
         guard x >= 0 else {
             return -reverse(-x)
