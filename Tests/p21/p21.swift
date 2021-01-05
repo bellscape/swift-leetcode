50eba86954aaeb73c72123043d9c2a4d1fda6636
bell <_>
2021-01-05 10:03:12 +0800

    add examples for ListNode problems

--- Tests/p21/p21.swift
+++ Tests/p21/p21.swift
@@ -2,12 +2,8 @@ import XCTest
 
 // Merge Two Sorted Lists
 class p21: XCTestCase {
-    public class ListNode {
-        public var val: Int
-        public var next: ListNode?
-        public init() { self.val = 0; self.next = nil; }
-        public init(_ val: Int) { self.val = val; self.next = nil; }
-        public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    func testExample() throws {
+        Judger.judge("p21", mergeTwoLists)
     }
 
 
